27ff5cccc0cb2acdda78d9eb8b7b986ff7009fb6
Ishasnath Gayan <ishanath.gayan@ancsis.com>
2024-11-02 01:44:20 +0530

    modified the endpoint keels to accept get parameter

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+dataString = "hello world";
+
 app.get("/", (req, res) => {
-  res.send("Hello, Worlds!: Node:server\n");
+  res.send("Hello, Worlds!: Node:server\ndataString: \n" + dataString);
 });
 
 app.get("/keels", (req, res) => {
-  res.send("Hello, Keels Worlds!\n");
+	const { strJson } = req.query;
+	if (!strJson) {
+		return res.status(400).json({ error: 'strJson is required' });
+	}
+	dataString = strJson;
+	res.send(`Parameter 1: ${strJson}`);
 });
 
 
