873ccd344a917b0d8bc359542ceb24f7185f1afc
Ishasnath Gayan <ishanath.gayan@ancsis.com>
2024-11-03 16:29:50 +0530

    initial cosmosDB support added. connection string was hidden in github secrets.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const cors = require('cors');
+const ServiceController = require("./controllers/appcontroller");
+//import { ServiceController } from "./controllers/appcontroller.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
 dataString = "hello world";
+app.use(cors());
+
+const appController = new ServiceController();
+
+appController.init().then(() => {
+	console.log('database and container created Successfully'); }).catch(() => {
+		console.log('database and container creation failed');
+		process.exit(1);
+	});
 
 app.get("/", (req, res) => {
   res.send("Hello, Worlds!: Node:server\ndataString: \n" + dataString);
---
+++ appconfig/config.js
@@ -0,0 +1,11 @@
+const appConfig = {
+
+	conneectionString: '',
+	databaseId: 'mcxdb',
+	containerId:'marketexpenses'
+
+};
+
+appConfig.conneectionString = process.env.COSMOS_CONNECTION_STRING;
+
+module.exports = appConfig;
